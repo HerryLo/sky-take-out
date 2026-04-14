e822076cb6f953079e8ea3200003342bb94802a5
HerryLo <herryloyopai@163.com>
2026-04-14 10:58:38 +0800

    feat: add SetmealVO class for detailed setmeal responses

--- sky-pojo/src/main/java/com/liuheng/vo/SetmealVO.java
+++ sky-pojo/src/main/java/com/liuheng/vo/SetmealVO.java
@@ -1,35 +1,21 @@
 package com.liuheng.vo;
 
 import lombok.Data;
+import com.liuheng.entity.SetmealDish;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
-/**
- * 套餐数据传输对象
- */
 @Data
 public class SetmealVO {
-
     private Long id;
-
-    private String name;
-
-    private BigDecimal price;
-
-    private String image;
-
-    private String description;
-
-    private LocalDateTime createTime;
-
-    private LocalDateTime updateTime;
-
-    private Integer status;
-
     private Long categoryId;
-
-    private String categoryName;
-
-    private List<DishVO> dishes;
+    private String name;
+    private BigDecimal price;
+    private Integer status;
+    private String description;
+    private String image;
+    private LocalDateTime createTime;
+    private String categoryName; // 关联分类名称
+    private List<SetmealDish> setmealDishes; // 关联菜品
 }
