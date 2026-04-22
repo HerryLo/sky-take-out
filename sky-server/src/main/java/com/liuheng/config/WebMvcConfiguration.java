c2fcc904416e103ee5d100a346e5a71447d6c1ae
HerryLo <herryloyopai@163.com>
2026-04-22 16:24:55 +0800

    feat: add user interceptor to WebMvcConfiguration

--- sky-server/src/main/java/com/liuheng/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/liuheng/config/WebMvcConfiguration.java
@@ -1,6 +1,7 @@
 package com.liuheng.config;
 
 import com.liuheng.interceptor.JwtTokenAdminInterceptor;
+import com.liuheng.interceptor.JwtTokenUserInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +14,19 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Autowired
     JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
 
+    @Autowired
+    JwtTokenUserInterceptor jwtTokenUserInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtTokenAdminInterceptor).addPathPatterns("/admin/**").excludePathPatterns("/admin/employee/login");
+        // 管理端拦截器
+        registry.addInterceptor(jwtTokenAdminInterceptor)
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/admin/employee/login");
+
+        // 用户端拦截器
+        registry.addInterceptor(jwtTokenUserInterceptor)
+                .addPathPatterns("/user/**")
+                .excludePathPatterns("/user/user/login", "/user/user/refresh");
     }
 }
